9855c9934bec6534a619aa64348d198d3e4d0716
BellaHuiruTan <89065746+BellaHuiruTan@users.noreply.github.com>
2023-03-06 01:17:05 +0800

    Delete mongodb/src/main/mongodb directory

--- mongodb/src/main/mongodb/model/repository/NewVendorAssessmentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.mongodb.model.repository;
-import com.example.mongodb.model.*;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-
-public interface NewVendorAssessmentRepository extends MongoRepository<NewVendorAssessment, String> {
-    
-}
