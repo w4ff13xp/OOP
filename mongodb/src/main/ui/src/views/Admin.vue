b3497ba4e2865f4f154dbe840f47cca06e43e09a
Zuhaili Amin <mohammadza.2020@scis.smu.edu.sg>
2023-03-13 22:04:33 +0800

    Deleting users from DB

--- mongodb/src/main/ui/src/views/Admin.vue
+++ mongodb/src/main/ui/src/views/Admin.vue
@@ -9,7 +9,7 @@
           >
 
             <div class="myForm mx-5 d-flex">
-              <div id="company-name" class="mx-5 w-50">{{ vendor.username }}</div>
+              <div id="company-name" class="mx-5 w-25">{{ vendor.username }}</div>
               <div id="accessRights">Current access right: {{ vendor.accessRights }}</div>
               <div class="form-check mx-5">
                 <input
@@ -47,7 +47,7 @@
               </div>
             </div>
 
-            <div>
+            <!-- <div @click="deleteVendors(vendor.id)">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="16"
@@ -60,7 +60,8 @@
                   d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"
                 />
               </svg>
-            </div>
+            </div> -->
+            <button class="btn btn-success" @click="deleteVendors(vendor.id)">Delete</button>
           </div>
       </div>
 
@@ -98,6 +99,19 @@ name:'Admin',
       })
       .catch ((error) => {
         console.log(error);
+      })
+    },
+    deleteVendors(vendorID) {
+      alert("Delete successful");
+      console.log(vendorID);
+      const axios = require('axios');
+      axios.delete('http://localhost:8080/vendors/' + vendorID)
+      .then((r)=>{
+        console.log(r);
+        location.reload();
+      })
+      .catch((e)=>{
+        console.log(e);
       })
     }
   },
