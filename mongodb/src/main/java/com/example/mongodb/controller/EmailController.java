7143d937b857377663ce60ca3e9bc6d3ffd00f8f
w4ff13xp <junhuilee98@yahoo.com.sg>
2023-03-27 15:03:19 +0800

    import cors

--- mongodb/src/main/java/com/example/mongodb/controller/EmailController.java
+++ mongodb/src/main/java/com/example/mongodb/controller/EmailController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.example.mongodb.model.Email;
 import com.example.mongodb.service.EmailService;
