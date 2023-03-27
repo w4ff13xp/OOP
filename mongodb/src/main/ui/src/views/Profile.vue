d15f1e6e1f1b14932fdb93293551b995ea9c4b73
bryan88844 <ongzsbryan@gmail.com>
2023-03-27 19:33:51 +0800

    Update Profile.vue

--- mongodb/src/main/ui/src/views/Profile.vue
+++ mongodb/src/main/ui/src/views/Profile.vue
@@ -1,5 +1,34 @@
-<template><h1>Profile</h1>
-    <!-- <HelloWorld /> -->
+<template>
+    <div class="container-fluid">
+      <div
+        class="mt-4 page-header min-height-300 border-radius-xl"
+
+      >
+        <span class="mask bg-gradient-success opacity-6"></span>
+      </div>
+      <div class="mx-4 overflow-hidden card card-body blur shadow-blur mt-n6">
+        <div class="row gx-4">
+
+          <div class="col-auto my-auto">
+            <div class="h-100">
+              <h5 class="mb-1">bry</h5>
+              <p class="mb-0 text-sm font-weight-bold">CEO / Co-Founder</p>
+            </div>
+          </div>
+          <div
+            class="mx-auto mt-3 col-lg-4 col-md-6 my-sm-auto ms-sm-auto me-sm-0"
+          ></div>
+        </div>
+        <div class="row mt-4">
+          <div class="col-auto">
+            Name:
+          </div>
+          <div class="col-auto">
+            Email:
+          </div>
+        </div>
+      </div>
+    </div>
   </template>
 
   <script>
